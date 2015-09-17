7d3de5985a61c6ec6181a690cce23530ec30e794
Cameron <cpc1007@g.rit.edu>
2015-09-17 16:02:02 -0400

    now converts article to mp3 file, working on flask app to launch on web

--- articleScraper.py
+++ articleScraper.py
@@ -6,14 +6,12 @@ def articleScraper(url):
     a.download()
     a.parse()
     articleString = a.text
-    tts = gTTS(text='Hello', lang='en')
-    tts.save("hello.mp3")
+    tts = gTTS(text=articleString, lang='en')
+    tts.save("article.mp3")
 
 
-def main():
+if __name__ == '__main__':
     #url = raw_input("URL: ")
     url = "http://www.nytimes.com/2015/09/17/us/texas-student-is-under-police-investigation-for-building-a-clock.html?ref=us&_r=0"
     articleScraper(url)
 
-
-main()
